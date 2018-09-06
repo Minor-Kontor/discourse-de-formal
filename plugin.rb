67bf9f7b53ca33e0589da907ca70401f2ee6f920
Fina <monkey@felinira.net>
2018-09-06 14:15:15 +0200

    Initial Commit

---
+++ plugin.rb
@@ -0,0 +1,5 @@
+# name: custom-locales
+# about: An example plugin for adding new locales.
+# version: 1.0
+
+register_locale("de-formal", name: "German (formal)", nativeName: "Deutsch (förmlich)", fallbackLocale: "de")
